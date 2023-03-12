16513589e570ae1c792211a99bfc64c5358d6bd3
minhduc184 <nmhwww3@gmail.com>
2023-03-12 22:13:15 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,26 +29,27 @@ $(document).ready(function(){
 
 const API_URL_MAINITEMS = "https://celena-restaurant-api.vercel.app/main-item";
 const URL = "https://celena-restaurant-api.vercel.app";
-
+getMainItemAPI(API_URL_MAINITEMS)
 async function getMainItemAPI(API_URL_MAINITEMS){
     const res = await axios.get(`${API_URL_MAINITEMS}`);
-    const data = await res.data.result;
+    const data = await res.data;
+
 
     showMainItem(data);
 }
 
-getMainItemAPI(API_URL_MAINITEMS)
+
 
 
 function showMainItem(data){
     let htmlCode =``;
     
-    data.map(function(main-item){
+    data.map(function(item){
         htmlCode+=`<div class="col-12 col-sm-4 col-md-4">
         <div class="logo">
           <div class="title">
-            <p>${main-item.description}</p>
-            <h3>${main-item.title}</h3>
+            <p>${item.description}</p>
+            <h3>${item.title}</h3>
           </div>
           <div class="detail">
             <p>FIND OUT MORE</p>
@@ -62,7 +63,6 @@ function showMainItem(data){
     });
 
 }
-getMainItemAPI();
 
 
 
